b84c9f16df18503b57a748acbd9fb434107c6511
michaelteye <michaelteyegyemah@gmail.com>
2022-01-02 11:40:20 +0000

    Numbers, number...waitLetter alternative

--- codewars111.js
+++ codewars111.js
@@ -18,3 +18,13 @@ const getNumbers = s => {
     ];
     return Math.round(arr.reduce((acc, n, i) => fns[(i-1)%fns.length](acc, n)));
   };
+
+//alternative
+function doMath(s){
+    const sign = ['+','-','*','/']; 
+    const tmp = s.split` `.sort((a,b) => a.replace(/\d/g,'').localeCompare( b.replace(/\d/g,'') ) || s.indexOf(a)-s.indexOf(b)).map((v,i) => v.replace(/[a-z]/g, '') );
+    let res = tmp[0];
+    for(let i = 1; i<tmp.length; i++)
+       res = eval(res + sign[(i-1)%sign.length] + tmp[i]);
+     return Math.round(res)
+}
