05d63a65a1b1f438e9f1141707a9b6e021f930da
michaelteye <michaelteyegyemah@gmail.com>
2022-01-05 11:40:20 +0000

    Numbers of Letters of Numbers alternative solution

--- codewars110.js
+++ codewars110.js
@@ -23,3 +23,35 @@ function digitToWord(i){
       }
     return words;
   }
+
+
+  // alternative
+
+  const digits = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+};
+
+function convertInteger(integer) {
+    return [...integer+""].map(digit => digits[digit]).join("");
+}
+
+function numbersOfLetters(integer) {
+    const arr = [convertInteger(integer)];
+
+    while (true) {
+        const item = arr[arr.length-1];
+        const int = convertInteger(item.length);
+        if (item !== int) arr.push(int);
+        if (item.length === int.length) break
+    }
+    return arr;
+}
