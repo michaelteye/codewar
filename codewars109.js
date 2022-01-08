0dd3d628d3083acb3e3db51d902b6c10a64d04bc
michaelteye <michaelteyegyemah@gmail.com>
2022-01-08 11:40:20 +0000

    The End is inevitable alternative solution

--- codewars109.js
+++ codewars109.js
@@ -21,3 +21,25 @@ inevitable-end (_ ∷ y ∷ ys) _ xs≢[] = inevitable-end (y ∷ ys) _ xs≢[]
 inevitable-end (y ∷ [])     _ xs≢[] with non-empty xs≢[]
 ... | _ ∷ _ = refl
 inevitable-end []           _ _     = refl
+
+// alternative solution
+
+{-# OPTIONS --safe #-}
+module InevitableEnd where
+
+open import Relation.Binary.PropositionalEquality
+open import Data.List
+open import Data.Empty
+open import Preloaded
+
+lemma : {A : Set} → ∀ (x : A) (xs : List A) → xs ≢ [] → last (x ∷ xs) ≡ last xs
+lemma x [] neq = ⊥-elim (neq refl)
+lemma x (x₁ ∷ xs) neq = refl
+
+append-not-nil : {a : Set} (ys xs : List a) → xs ≢ [] → ys ++ xs ≢ []
+append-not-nil [] xs neq = neq
+append-not-nil (x ∷ xs₁) xs neq = λ ()
+
+inevitable-end : {a : Set} (ys xs : List a) → xs ≢ [] → last (ys ++ xs) ≡ last xs
+inevitable-end [] xs neq = refl
+inevitable-end (x ∷ ys) xs neq = trans (lemma x (ys ++ xs) (append-not-nil ys xs neq)) (inevitable-end ys xs neq)
