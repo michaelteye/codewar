4ee2749ba4789dfb18bd8809be91e357ee19089c
michaelteye <michaelteyegyemah@gmail.com>
2022-01-10 11:40:20 +0000

    Simple Counter alternative solution

--- codewars108.js
+++ codewars108.js
@@ -30,3 +30,12 @@ Y = \ f . M (\ x . f (x x))
 go = Y (\ g n b . b (g (succ n)) n)
 
 counter = go 0
+
+// alternative solution
+
+zero = \ a b . b
+succ = \ n f x . f (n f x)
+
+y = \ f . (\ x . f (x x)) (\ x . f (x x))
+
+counter = y (\ count n b . b (count (succ n)) n) zero
