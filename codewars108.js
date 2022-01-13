765c4a509591c5d75a923db0a897c35a3b8d5d9c
michaelteye <michaelteyegyemah@gmail.com>
2022-01-13 11:40:20 +0000

    Simple Counter alternative solution

--- codewars108.js
+++ codewars108.js
@@ -20,3 +20,13 @@ succ = \ n f x . f (n f x)
 fix = \ f . (\ x . f (x x)) (\ x . f (x x))
 go = fix (\ go n continue . continue (go (succ n)) n)
 counter = go 0
+
+// alternative solution
+succ = \ n f x . f (n f x)
+
+M = \ f . f f
+Y = \ f . M (\ x . f (x x))
+
+go = Y (\ g n b . b (g (succ n)) n)
+
+counter = go 0
