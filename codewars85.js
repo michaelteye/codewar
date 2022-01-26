6bd2f0e59f8bc4806977c7453477a590949f3541
michaelteye <michaelteyegyemah@gmail.com>
2022-01-26 11:40:20 +0000

    Disemvowel Trolls

--- codewars85.js
+++ codewars85.js
@@ -5,20 +5,7 @@ function sortList (sortBy, list) {
   }
 
 // Alternative
-function sortList (sortBy, list) {
-    return list.sort(function(a, b) {    
-      if (a[sortBy] < b[sortBy]) {
-        return 1;
-      }
-      else if (a[sortBy] < b[sortBy]) {
-        return -1;
-      }
-      else {
-        return 0;
-      }
-      
-    });
-  }
+
 // Alternative solutions
 
   function sortList (sortBy, list) {
