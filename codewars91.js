a3fd583ababa22587eb6d69182af96f117f9f302
michaelteye <michaelteyegyemah@gmail.com>
2022-02-15 11:40:20 +0000

    Extending JavaScript Objects: Simple Math Methods

--- codewars91.js
+++ codewars91.js
@@ -17,3 +17,15 @@ Number.prototype.add= function(n){
   Number.prototype.sqr= function(){
     return Math.sqrt(this)
   }
+
+// Alternative
+(function(ops){
+    Object.keys( ops ).forEach( op => { Number.prototype[op] = function(x){ return ops[op](this,x) } })
+  })( {
+    "add" : (a,b) => a + b,
+    "sub" : (a,b) => a - b,
+    "mul" : (a,b) => a * b,
+    "div" : (a,b) => a / b,
+    "pow" : (a,b) => Math.pow(a,b),
+    "sqr" : a => Math.sqrt(a),
+  } );
