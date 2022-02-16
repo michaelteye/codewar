28d9c6d92592de10732888d911ccc5f5f4ce02ef
michaelteye <michaelteyegyemah@gmail.com>
2022-02-16 11:40:20 +0000

    Extending JavaScript Objects: Simple Math Methods

--- codewars91.js
+++ codewars91.js
@@ -1,6 +1,19 @@
-String.prototype.reverse=function(){
-    var s='';
-    for (var i=0; i<this.length; ++i)
-      s+=this[this.length-1-i];
-    return s;
+// Write your code here...
+Number.prototype.add= function(n){
+    return this + n
+  }
+  Number.prototype.sub= function(n){
+    return this - n
+  }
+  Number.prototype.mul= function(n){
+    return this * n
+  }
+  Number.prototype.div= function(n){
+    return this / n
+  }
+  Number.prototype.pow= function(n){
+    return  this ** n
+  }
+  Number.prototype.sqr= function(){
+    return Math.sqrt(this)
   }
