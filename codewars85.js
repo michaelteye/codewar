ef48de2d76dda6d0aa4bd2f7b5b2f54e10714a0a
michaelteye <michaelteyegyemah@gmail.com>
2022-02-28 11:40:20 +0000

    Return a sorted list of objects alternative method

--- codewars85.js
+++ codewars85.js
@@ -3,3 +3,19 @@ function sortList (sortBy, list) {
       return a[sortBy] < b[sortBy];  
     })
   }
+
+// Alternative
+function sortList (sortBy, list) {
+    return list.sort(function(a, b) {    
+      if (a[sortBy] < b[sortBy]) {
+        return 1;
+      }
+      else if (a[sortBy] < b[sortBy]) {
+        return -1;
+      }
+      else {
+        return 0;
+      }
+      
+    });
+  }
