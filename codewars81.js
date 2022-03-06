5c4919064e6f09cf3c491b5cbe00efc4907d3dfb
michaelteye <michaelteyegyemah@gmail.com>
2022-03-06 11:40:20 +0000

    fake binary using the map function

--- codewars81.js
+++ codewars81.js
@@ -17,3 +17,16 @@ function fakeBin(x){
 function fakeBin(x) {
     return x.split('').map(n => n < 5 ? 0 : 1).join('');
 }
+// Alternative solution
+function fakeBin(str){
+    var newStr = "";
+    for(var i=0;i<str.length;i++){
+      if(Number(str[i])>=5){
+        newStr += "1"
+      }
+      else{
+        newStr += "0";
+      }
+    }
+    return newStr;
+  }
