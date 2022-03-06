899eb58da21f8291173b329a28ae590240ef19e3
michaelteye <michaelteyegyemah@gmail.com>
2022-03-06 11:40:20 +0000

    Beginner Series #1 School Paperwork using different approach

--- codewars82.js
+++ codewars82.js
@@ -7,4 +7,10 @@ function paperwork(n, m) {
   }
     return mute
   }
-  
+// alternative solution
+function paperwork(n, m) {
+    if (m < 0 || n < 0) {
+      return 0;
+    }
+    return m * n;
+  }
