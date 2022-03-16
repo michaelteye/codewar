038c6a67df1a45909453d14df52e1babe1b06f82
michaelteye <michaelteyegyemah@gmail.com>
2022-03-16 11:40:20 +0000

    Count of positive/sum of negatives using the arrow functions

--- codewars77.js
+++ codewars77.js
@@ -23,3 +23,8 @@ function countPositivesSumNegatives(input){
     return [mynumber.length, myneg]
     
   }
+  // Alternate solution using the arrow functions
+  
+  function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
