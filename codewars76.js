87a93f43acd475c96a91093606781e07ac37f82a
michaelteye <michaelteyegyemah@gmail.com>
2022-03-17 11:40:20 +0000

    using the date

--- codewars76.js
+++ codewars76.js
@@ -18,3 +18,11 @@ function past(h, m, s){
     
     return hours + minutes + seconds;
   }
+
+  // Alternate
+  function past(h, m, s){
+    const setTime = new Date().setHours(h, m, s);
+    const midnight = new Date().setHours(0, 0, 0);
+    
+    return Math.round(setTime - midnight);
+  }
