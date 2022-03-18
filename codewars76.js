ae0dab37336ec545d297866cf7cd4702652d8aed
michaelteye <michaelteyegyemah@gmail.com>
2022-03-18 11:40:20 +0000

    alternate solution

--- codewars76.js
+++ codewars76.js
@@ -9,3 +9,12 @@ function past(h, m, s){
 function past(h, m, s){
     return ((h*3600)+(m*60)+s)*1000;
   }
+
+// alternate
+function past(h, m, s){
+    var hours = h * 60 * 60 * 1000;
+    var minutes = m * 60 * 1000;
+    var seconds = s * 1000;
+    
+    return hours + minutes + seconds;
+  }
