f042bf5d75cc794d8916ebfc2c21f42dc1471c21
michaelteye <michaelteyegyemah@gmail.com>
2022-04-03 11:40:20 +0000

    adding changes to the welcome code

--- codewars68.js
+++ codewars68.js
@@ -46,3 +46,28 @@ function greet(lang) {
   'swedish': 'Valkommen',
   'welsh': 'Croeso'
   };
+
+  // Alternate solution
+  function greet(language) {
+    var GreetingsDB = {
+      english: 'Welcome',
+      czech: 'Vitejte',
+      danish: 'Velkomst',
+      dutch: 'Welkom',
+      estonian: 'Tere tulemast',
+      finnish: 'Tervetuloa',
+      flemish: 'Welgekomen',
+      french: 'Bienvenue',
+      german: 'Willkommen',
+      irish: 'Failte',
+      italian: 'Benvenuto',
+      latvian: 'Gaidits',
+      lithuanian: 'Laukiamas',
+      polish: 'Witamy',
+      spanish: 'Bienvenido',
+      swedish: 'Valkommen',
+      welsh: 'Croeso'
+    }, 
+    defaultLanguage = 'english';
+    return GreetingsDB[language] || GreetingsDB[defaultLanguage];
+  }
