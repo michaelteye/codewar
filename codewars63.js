7cc0268dd75c57a9511e88a0e9c92ca45d66e091
michaelteye <michaelteyegyemah@gmail.com>
2022-04-23 11:40:20 +0000

    counting sheep

---
+++ codewars63.js
@@ -0,0 +1,10 @@
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    var num = 0;
+    
+    for(var i = 0; i < arrayOfSheep.length; i++)
+      if(arrayOfSheep[i] == true)
+        num++;
+        
+    return num;
+  }
