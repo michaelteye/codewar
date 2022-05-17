bc18af2a476f0ba13841a726613976f16ad8cb18
michaelteye <michaelteyegyemah@gmail.com>
2022-05-17 23:31:09 +0000

    methods of arrayObject concat() and join()

---
+++ codewars.23.js
@@ -0,0 +1,21 @@
+// Coding in function bigToSmall. function accept 1 parameter arr(2D number array).
+
+// Your task is: First to all, refer to the example above, flat output arr to a one-dimensional array.
+
+// And then sort array in descending order.
+
+// Finally, use the separator ">" to connect the elements into a string.
+
+// Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
+
+
+
+function bigToSmall(arr){
+    //coding here...
+    
+    var results =[]
+    return results.concat(...arr).sort((a,b)=> b-a).join(">")
+  
+    
+    
+  }
