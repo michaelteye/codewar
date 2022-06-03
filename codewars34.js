feb6d79dad56efe72431cc8f14b105d89e0dcbba
michaelteye <michaelteyegyemah@gmail.com>
2022-06-03 22:28:46 +0000

    adding description to codewar34

--- codewars34.js
+++ codewars34.js
@@ -1,3 +1,10 @@
+// Define a circular object circular such that the following are true:
+
+// circular.value === "Hello World"
+// circular === circular.self
+// circular.self === circular.self.self
+// ... and so on.
+
 var foo;
 
 foo = function(arg) {
