8222c7bfbdc5e97b279aa47949338efb5c20d429
michaelteye <michaelteyegyemah@gmail.com>
2022-06-08 22:15:20 +0000

    Working with Number, Replace and template literal in strings

---
+++ codewars39.js
@@ -0,0 +1,7 @@
+// Removing white spacing in string
+
+function noSpace(x){
+    let tryit = x
+    return tryit.replace(/\s/g, '')
+  
+  }
---
+++ codewars40.js
@@ -0,0 +1,6 @@
+// Convert a String to a Number!
+
+const stringToNumber = function(str){
+    let through = Number(str)
+    return through;
+  }
