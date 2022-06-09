8d175ccbc861f68fc572f355ec73da93eba7ab61
michaelteye <michaelteyegyemah@gmail.com>
2022-06-09 23:14:35 +0000

    Convert a string to an array

---
+++ codewars42.js
@@ -0,0 +1,10 @@
+function booleanToString(b){
+    //   let isSetToTrue = (b.toLowerCase() === 'true')
+    //   return isSetToTrue
+      if(Boolean(b)){
+        return "true"
+      }else{
+        return "false"
+      }
+      
+}
---
+++ codewars43.js
@@ -0,0 +1,9 @@
+function stringToArray(string){
+
+    //   let tryit = []
+      let metoo = string.split(" ")
+      
+      return metoo
+      
+    
+}
