5ef03431abfde30e998fae6b6c059a95cc67e781
michaelteye <michaelteyegyemah@gmail.com>
2022-06-11 11:34:59 +0000

    Stringy Strings

---
+++ codewars44.js
@@ -0,0 +1,7 @@
+function sumStr(a,b) {
+    let coup = Number(a)
+    let soup = Number(b)
+    let mum = coup + soup
+    return mum.toString()
+    
+  }
---
+++ codewars45.js
@@ -0,0 +1,11 @@
+function stringy(size) {
+    // your code here  pa
+    let tryit =''
+    for(let i=1; i<= size; i++){
+      tryit+=i%2
+        
+      
+    }
+    return tryit
+    
+  }
