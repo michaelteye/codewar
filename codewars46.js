4fa575092a97348d07d8fb64de8e269b330d1fc8
michaelteye <michaelteyegyemah@gmail.com>
2022-06-12 22:32:19 +0000

    checking palindrome in both number and string

---
+++ codewars46.js
@@ -0,0 +1,20 @@
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+// Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+
+// Examples(Input ==> Output)
+
+
+function isPalindrome(line) {
+    let tryit = line.toString()
+    let mysplit = tryit.split('')
+    let myreverse= mysplit.reverse()
+    let myjoin = myreverse.join('')
+    if(myjoin ==  line){
+      return true
+    }else{
+      return false
+    }
+  
+  //   return undefined;
+  }
