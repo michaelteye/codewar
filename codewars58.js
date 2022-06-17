8aa10707bbb077ef914be0d75720fe4e84f1e55e
michaelteye <michaelteyegyemah@gmail.com>
2022-06-17 21:03:13 +0000

    Returning negative integer

---
+++ codewars58.js
@@ -0,0 +1,10 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+function makeNegative(num) {
+    // Code?
+    let mynum = -num
+    if(mynum===num){
+      return mynum
+    }else{
+      return mynum
+    }
+  }
