27e9f28b3e83735f0c9c42e12e32e1fcbf59f8bf
michaelteye <michaelteyegyemah@gmail.com>
2022-06-24 11:40:20 +0000

    Basic Mathematical Operations

---
+++ codewars71.js
@@ -0,0 +1,32 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  let myadd = value1 + value2
+  let mysub = value1 - value2
+  let mymul = value1 * value2
+  let mydiv = value1 / value2
+  if(operation === '+'){
+    return myadd
+  }
+  if(operation === '-'){
+    return mysub
+  }
+  if(operation === '*'){
+    return mymul
+  }
+  if(operation === '/'){
+    return mydiv
+  }
+}
