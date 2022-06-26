edc235bfcf4f51ed8c9ca2464a23576d63512f40
michaelteye <michaelteyegyemah@gmail.com>
2022-06-26 11:40:20 +0000

    doubling the integer and returning in it using the arrow function

--- codewars73.js
+++ codewars73.js
@@ -1,5 +1,7 @@
 // Code as fast as you can! You need to double the integer and return it.
 function doubleInteger(i) {
-    let myi = i*i
+    let myi = i*2
     return myi;
   }
+// Alternative
+const doubleInteger = i => 2 * i;
