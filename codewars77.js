598dcd695d8392ecea5a298b0fac61830cf1d5c0
michaelteye <michaelteyegyemah@gmail.com>
2022-06-29 22:53:14 +0000

    Count of positive/sum of negatives

---
+++ codewars77.js
@@ -0,0 +1,25 @@
+// codewars=77
+// Given an array of integers.
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input){
+  
+    if(input == null || input.length == 0){
+      return []
+    }
+    let mynumber = []
+    let myneg = 0
+    for(let i = 0; i < input.length; i++){
+      if(input[i] > 0){
+        mynumber.push(input[i])
+      }
+      if(input[i]< 0){
+        myneg += input[i]
+      }
+    }
+    return [mynumber.length, myneg]
+    
+  }
