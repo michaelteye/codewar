4381b26a00a81966d357d97f192958c36be1e861
michaelteye <michaelteyegyemah@gmail.com>
2022-07-02 11:40:20 +0000

    more information to class like objects

--- codewars84.js
+++ codewars84.js
@@ -10,3 +10,31 @@ class Animal{
   var dog = new Animal('Max', 'farm')
   dog.name ="checp"
   console.log(dog.toString())
+
+//Alternative solution
+var Animal = function (name, type) {
+    var name = name;
+    var type = type;
+    this.toString = function () {
+      return this.name + ' is a ' + this.type;
+    };
+    
+     Object.defineProperty(this, 'name', {
+      get: function () {
+        return name;
+      },
+      set: function (n) {
+        name = n;
+      }
+    })
+    
+    Object.defineProperty(this, 'type', {
+      get: function () {
+        return type;
+      },
+      set: function (t) {
+        type = t;
+      }
+    })
+   
+  } 
