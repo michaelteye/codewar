111ef2f1699b814042b7da66bce603bced8a079e
michaelteye <michaelteyegyemah@gmail.com>
2022-07-07 11:40:20 +0000

    checking for right solution

--- codewars86.js
+++ codewars86.js
@@ -17,3 +17,17 @@ function combine(...obj) {
     })
     return finalObj  
   }
+// Alternative solution
+function combine() {
+    let result = {};
+    for (let obj of arguments) {
+        for (let property in obj) {
+            if (property in result) {
+                result[property] += obj[property];
+            } else {
+                result[property] = obj[property];
+            }
+        }
+    }
+    return result;
+}
