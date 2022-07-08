ddfc62542e6f92749a741638fd6d1e02e8cc916f
michaelteye <michaelteyegyemah@gmail.com>
2022-07-08 11:40:20 +0000

    alternative solution to combine objects

--- codewars86.js
+++ codewars86.js
@@ -4,3 +4,16 @@ const combine = (...params) => params.reduce((a, b) => {
     for (const x in b) { a[x] = x in a ? a[x] + b[x] : b[x] };
     return a;
    }, {});
+
+// Alternative method
+function combine(...obj) {
+    let objArr = [].concat(...obj)
+    let finalObj = {}
+    objArr.forEach(object => { 
+      for(key in object){
+        if(finalObj[key]) finalObj[key] = finalObj[key] + object[key] 
+        else finalObj[key] = object[key] 
+      }
+    })
+    return finalObj  
+  }
