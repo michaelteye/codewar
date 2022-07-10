f4d4489f1c8b6824ab031991f10964fb594c4d55
michaelteye <michaelteyegyemah@gmail.com>
2022-07-10 11:40:20 +0000

    extending javascript objects alternative method

--- codewars89.js
+++ codewars89.js
@@ -29,3 +29,14 @@ Array.prototype.first = function() {
     const copy = [...this];
     return copy.pop();
   };
+
+// Alternative solution
+Array.prototype.first = function(){
+    if(this.length == 0) return undefined;
+    return this[0];
+  }
+  
+  Array.prototype.last = function(){
+    if(this.length == 0) return undefined;
+    return this.slice(-1)[0];
+  }
