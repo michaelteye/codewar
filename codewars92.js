58979d6b44de62ea9ef282c648632ac59cb627f9
michaelteye <michaelteyegyemah@gmail.com>
2022-07-12 22:45:59 +0000

    Extending JavaScript Objects: Contains Method

---
+++ codewars92.js
@@ -0,0 +1,13 @@
+// Your task is to extend JavaScript String object with a .contains(substr) method, so you can check if the string contains a substring.
+
+// By default, it has to be a case-insensitive check. But you also have to provide a second optional bool parameter, so the method would be able to do a case-sensitive check as well.
+
+// 'Hello, World!'.contains('Hel');         // true
+// 'Hello, World!'.contains('wor');         // true
+// 'Hello, World!'.contains('wor', true);   // false (case-sensitive check)
+// 'Hello, World!'.contains('a');           // false
+
+String.prototype.contains = function(substr, sensitive = false)
+{
+  return sensitive ? this.includes(substr) :this.toLowerCase().includes(substr.toLowerCase());
+}
