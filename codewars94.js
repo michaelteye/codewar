e9651cb64bab2767babca808496f26b6795b796b
michaelteye <michaelteyegyemah@gmail.com>
2022-07-17 11:40:20 +0000

    multiples of 3 or 5 in kata 6 alternative solution

--- codewars94.js
+++ codewars94.js
@@ -8,3 +8,8 @@ function solution(number){
     }
     return sum;
 }
+// alternative solution
+function solution(number){
+    var n3 = Math.floor(--number/3), n5 = Math.floor(number/5), n15 = Math.floor(number/15);
+    return (3 * n3 * (n3 + 1) + 5 * n5 * (n5 + 1) - 15 * n15 * (n15+1)) /2;
+  }
