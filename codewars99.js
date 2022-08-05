112c2763480e883cc7f2ec2ff6d6c092bf4b4de0
michaelteye <michaelteyegyemah@gmail.com>
2022-08-05 00:29:14 +0000

    return values which sums up to zero

---
+++ codewars99.js
@@ -0,0 +1,12 @@
+function sumzero(arr){
+    for(let i =0; i<arr.length; i++){
+        for(let j = i+1; j< arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [ arr[i], arr[j]];
+
+            }
+        }
+    }
+}
+
+console.log(sumzero([-4,-3,-2,-1,0,1,2,5]))
