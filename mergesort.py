bc0d15f294170ffec22a81197920955b493d33a0
michaelteye <michaelteyegyemah@gmail.com>
2022-08-11 23:00:23 +0000

    update

--- mergesort.py
+++ mergesort.py
@@ -1,9 +1,15 @@
-n = list (map(int, input("\nInput sizes: ").strip().split()))
+# n = list (map(int, input("\nInput sizes: ").strip().split()))
 
-a = list (map(int, input("\nEnter the numbers: ").strip().split()))[:n[0]]
-b = list (map(int, input("\nEnter the numbers: ").strip().split()))[:n[1]]
+# a = list (map(int, input("\nEnter the numbers: ").strip().split()))[:n[0]]
+# b = list (map(int, input("\nEnter the numbers: ").strip().split()))[:n[1]]
 
-a.extend(b)
-sor = sorted(a)
-print(a, sep = "\n")
+# a.extend(b)
+# sor = sorted(a)
+# print(a, sep = "\n")
+
+x= 0
+while x <= 0:
+    if x%2==0:
+        print(x)
+    x+=1
 
