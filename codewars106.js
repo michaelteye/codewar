3ea268f015cbca7272b0b41f8f1cf2910a823adb
michaelteye <michaelteyegyemah@gmail.com>
2022-08-28 11:40:20 +0000

    Advance Simple Pig Latin alternative solution

--- codewars106.js
+++ codewars106.js
@@ -27,3 +27,28 @@ function translate(sentence) {
       return cap + rest + cons + 'ay';
     });
   };
+
+// alternative solution
+function translate(sen) {
+    //this regex will select the word in 3 parts: match= whole word, p1= consonant part, p2= the rest
+    //p1 and p2 would be undefined, if the word starts with a vowel
+    return sen.replace(/\b([^aeiouAEIOU\s][^aeiouyAEIOU\s]*)(\w*)\b|\b[aeiouAEIOU]+\w*\b/gm, scramble);
+
+    function scramble(match, p1, p2) {
+        var word;
+        //if the word starts consonant, put first part at the end and append "ay"
+        if (p1) {
+          word = p2 + p1.toLowerCase()+ "ay";//consonant part in lowercase
+            //if the word starts with upper case consonant, put first letter in new word in upper case
+            //and make p2 lowercase
+            if (p1.charAt(0).toUpperCase() == p1[0]) {
+                word = word.charAt(0).toUpperCase() + word.slice(1);
+            }
+            return word;
+        }
+        //if the word starts with a vowel, append "way"
+        if (!p1) {
+            return match + "way";
+        }
+    }
+}
