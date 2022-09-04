bfd83ff313a33f558dc400aa9b895c9528dfe6d7
Teye Michael <michaelteyegyemah@gmail.com>
2022-09-04 11:24:10 +0000

    alternative solution sum of arrays

--- codewar117.js
+++ codewar117.js
@@ -9,3 +9,11 @@ function sum (array) {
   
 }
 console.log(sum(array));
+
+// alternative solution
+function mergeArrays(arr1, arr2) {
+    return arr1
+      .filter((item) => !arr2.includes(item))
+      .concat(arr2)
+      .sort((a, b) => a - b)
+  }
