210d9bf1b0ed10334f9aea9d6362674ca0592955
Teye Michael <michaelteyegyemah@gmail.com>
2022-09-05 11:57:22 -0700

    alternative solution

--- codewars113.js
+++ codewars113.js
@@ -10,3 +10,18 @@ function solution(str){
       }
     return result;
   }
+
+// Alternative
+function solution(str){
+  let result = []
+
+  for (let i = 0; i < str.length; i += 2) {
+    if (i < str.length - 1) {
+      result.push(str[i] + str[i + 1])
+    } else {
+     result.push(str[i] + '_') 
+    }
+  }
+  
+  return result
+}
