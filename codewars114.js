b6f5fb877ff19db9b670638e45378c5bb87dc11e
Teye Michael <michaelteyegyemah@gmail.com>
2022-09-09 10:33:16 +0000

    consective string

--- codewars114.js
+++ codewars114.js
@@ -10,3 +10,22 @@ function longestConsec(strarr, k) {
     }
     return longest;
 }
+
+//Alternative Solution
+
+// function to access two characters in a string
+
+function mysolution(strin){
+  let ouput = []
+
+  for(let i = 0; i < strin.length; i+=2){
+      if(i < strin.length-1){
+          ouput.push(strin[i] + strin[i + 1])
+      }
+      else{
+          ouput.push(strin[i] + '_')
+      }
+  }
+  return ouput
+}
+console.log(mysolution("Star with the boy"))
