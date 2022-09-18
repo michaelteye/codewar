30371eb576bf4effcca1d459d8fe51d4f4607855
Teye Michael <michaelteyegyemah@gmail.com>
2022-09-18 11:19:24 +0000

    move in square strings

---
+++ codewars121.js
@@ -0,0 +1,13 @@
+function vertMirror(str) {
+    return str.split('\n')
+      .map(line => line.split('').reverse().join(''))
+      .join('\n')
+}
+function horMirror(str) {
+    return str.split('\n')
+      .reverse()
+      .join('\n')
+}
+function oper(fct, s) {
+    return fct(s)
+}
