0c463b234f47a94b5b43a2263c890364d842de19
Teye Michael <michaelteyegyemah@gmail.com>
2022-09-20 10:21:20 +0000

    return the missing numbers of a given arrays

---
+++ codewars122.js
@@ -0,0 +1,13 @@
+// write a function that return the missing numbers of a given arrays
+
+function retunMissingArray(arr1, arr2){
+    let newArr1 = arr1.sort()
+    let newArr2 = arr2.sort()
+    for(let i = 0; i <= newArr1.length; i++){
+        if(newArr1[i] != newArr2[i]){
+            return (newArr2[i])
+        }
+    }
+}
+
+console.log(retunMissingArray([1,3,5,7],[2,4,7,5]))
