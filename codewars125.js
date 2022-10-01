b3cd3821697ebc0c155dd260b86890ad29ebd536
Teye Michael <michaelteyegyemah@gmail.com>
2022-10-01 11:10:12 +0000

    alternative solution Substituting Variables Into Strings: Padded Numbers

--- codewars125.js
+++ codewars125.js
@@ -4,3 +4,8 @@ function solution(value){
     while(value.length < size) value = "0" + value
     return `Value is ${value}`
 }
+
+// Alternative solution
+function solution(value){
+    return `Value is ${value.toString().padStart(5,'0')}`
+}
